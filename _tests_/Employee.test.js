f5af54ba355720839d6fd37fc9acfb7c1b0fc3fb
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-04-08 15:40:07 -0500

    Testing employee test

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,5 +1,8 @@
 const Employee = require("../index.js")
 
 describe("Employee", () => {
-    it()
-})
+    it("should return an object containing 'name', 'ID', & 'email' properties, when called with the 'new' keyword", () => {
+        const obj = new Employee();
+        expect("name" in obj).toEqual(expect.any(typeof(String)))
+    })
+});
